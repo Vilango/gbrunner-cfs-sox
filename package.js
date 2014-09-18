2a10ac93de293c287a7da195b591f40716ef11a2
Gerwin Brunner <gerwin.brunner@gmail.com>
2014-09-18 15:17:38 +0200

    switched to adapted sox version

--- package.js
+++ package.js
@@ -1,11 +1,11 @@
 Package.describe({
-  version: '0.0.1',
+  version: '0.0.2',
   summary: "Adds `sox` to scope with the ability to perform audio manipulation",
   git: "https://github.com/Vilango/gbrunner-cfs-sox"
 });
 
 Npm.depends({
-  sox: "0.1.0"
+  "vilango-sox": "0.1.2"
 });
 
 //also requires that you install the sox package on your server
